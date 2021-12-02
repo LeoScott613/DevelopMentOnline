647b9e648deb857b86cc050979d8ac2097a61779
Leoscott <PRCSteven06@gmail.com>
2021-12-02 21:05:30 +0800

    2105

---
+++ Lib/project/main.cpp
@@ -0,0 +1,18 @@
+#include "self.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+proj inpu()
+{
+    proj inp;
+    cin>>inp.name;
+    return inp;
+}
+int main()
+{
+    vector<proj> lhx;
+    lhx.push_back(inpu());
+    vector<proj>::iterator ilhx=lhx.begin();
+    std::cout<<(*ilhx).name<<std::endl;
+    return 0;
+}
---
+++ Lib/project/self.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+class proj{
+public:
+    std::string name;
+};
