f94571c946b983b454a4b796ab24c5c60027acc8
Leoscott <PRCSteven06@gmail.com>
2021-12-03 10:45:10 +0800

    Signed-off-by: Leoscott <PRCSteven06@gmail.com>

--- CPP/test3/test2.cpp
+++ CPP/test3/test2.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
+char const addsig = '+', equalsig='=';
 int recursion(int num)
 {
     /*static*/ int sum;
     if (num >= 1)
         sum = num + recursion(num - 1);
-    // cout<<sum<<' ';
+    int i(1);
+    for (; i < num; i++)
+        cout << i << addsig;
+    cout << i << equalsig << sum << endl;
+    //cout << sum << endl;
     return sum;
 }
 int main()
 {
     int n;
     cin >> n;
-    cout << recursion(n);
+    recursion(n);
     return 0;
 }
