8b4327c08b382fc44fd798cd2b243671edbdb291
Leoscott <PRCSteven06@gmail.com>
2021-12-10 13:05:06 +0800

    .

--- CPP/test4/test4.c
+++ CPP/test4/test4.c
@@ -5,27 +5,19 @@ int main(int argc, char *argv[])
     char *tempo[100], min[100];
     strcpy(min, argv[1]);
     int i, j;
-    // printf("%d",argc);
-    //  puts("ok1");
-    //  puts(min);
     for (i = 1; i < argc; i++)
     {
         memset(min,'\255',sizeof(min));
-        puts(min);
-        // puts("okfor");
         for (j = i; j < argc; j++)
             if (strcmp(argv[j], min) < 0)
             {
                 strcpy(min, argv[j]);
-                // puts(min);
             }
-        puts(min);
         tempo[i] = min;
     }
-    // puts("ok2");
-    /*
+    
     for (i = 1; i < argc; i++)
         puts(tempo[i]);
-    */
+    
     return 0;
 }
