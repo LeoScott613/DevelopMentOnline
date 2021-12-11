c133f0994b28d86698011168a880ab380ed6442f
Leoscott <PRCSteven06@gmail.com>
2021-12-11 09:56:50 +0800

    .

--- CPP/test4/test5.c
+++ CPP/test4/test5.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int main()
 {
-    
+    char *p = (char *)malloc(100);
+    char str[1000]="";
+    do
+    {
+        free(p);
+        p = (char *)malloc(100);
+        gets(p);
+        printf("p is:%s//strcmp=%d\n", p, strcmp(p, ""));
+        if (strcmp(p, ""))
+            strcat(str, p);
+    } while (strcmp(p, ""));
+    puts(str);
+    printf("%s", str);
+    //putchar(str[2]);
+    return 0;
 }
