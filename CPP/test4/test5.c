6b30f1a7b2902ab1f0336ff8daf74c747c7e27d3
LeoScott613 <87657181+LeoScott613@users.noreply.github.com>
2021-12-13 16:55:07 +0800

    .

--- CPP/test4/test5.c
+++ CPP/test4/test5.c
@@ -10,12 +10,11 @@ int main()
         free(p);
         p = (char *)malloc(100);
         gets(p);
-        printf("p is:%s//strcmp=%d\n", p, strcmp(p, ""));
+        //printf("p is:%s\n", p);
         if (strcmp(p, ""))
             strcat(str, p);
     } while (strcmp(p, ""));
     puts(str);
     printf("%s", str);
-    //putchar(str[2]);
     return 0;
 }
