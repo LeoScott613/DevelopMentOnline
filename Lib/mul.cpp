5bd8b30ce621f559aa753fd3258270d6848773ba
Leoscott <PRCSteven06@gmail.com>
2022-01-28 12:35:59 +0800

    .

--- Lib/mul.cpp
+++ Lib/mul.cpp
@@ -1,28 +1,30 @@
-#include<iostream>
-#include<cstring>
+#include <cstring>
+#include <iostream>
+char a1[10001],b1[10001];
+int a[10001],b[10001],c[10001],len;
 using namespace std;
-char a1[50001],b1[50001];
-int a[50001],b[50001],i,x,len,j,c[50001];
-int main ()
+int main()
 {
-    cin >>a1 >>b1;//读入两个数
-    a[0]=strlen(a1);b[0]=strlen(b1);//计算长度
-    for(i=1;i<=a[0];++i)
-        a[i]=a1[a[0]-i]-'0';//将字符串转换成数字
-    for(i=1;i<=b[0];++i)
+    cin>>a1>>b1;
+    a[0]=strlen(a1);
+    b[0]=strlen(b1);
+    for(int i(1);i<=a[0];i++)
+        a[i]=a1[a[0]-i]-'0';
+    for(int i(1);i<=b[0];i++)
         b[i]=b1[b[0]-i]-'0';
-    for(i=1;i<=a[0];++i)
-        for(j=1;j<=b[0];++j)
-            c[i+j-1]+=a[i]*b[j];//按乘法
-    len=a[0]+b[0];              //原理进行高精乘
-    for (i=1;i<len;++i)
-        if (c[i]>9)
+    for(int i(1);i<=a[0];i++)
+        for(int j(1);j<=b[0];j++)
+            c[i+j-1]+=a[i]*b[j];
+    len=a[0]+b[0];
+    for(int i(1);i<len;i++)
+        if(c[i]>9)
         {
-            c[i+1]+=c[i]/10;c[i]%=10;
-        }//进位
+            c[i+1]+=c[i]/10;
+            c[i]%=10;
+        }
     while(c[len]==0&&len>1)
-        len--;//判断位数
-    for(i=len;i>=1;--i)
-        cout<<c[i];//输出
+        len--;
+    for(int i(len);i>=1;i--)
+        cout<<c[i];
     return 0;
 }
---
+++ Lib/stl_mul.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+const string test("succeed!");
+int main()
+{
+    ios::sync_with_stdio(false);
+    string m1,m2;
+    cin>>m1>>m2;
+    string::iterator i1(m1.begin()),i2(m2.begin());
+    vector<int> mul1,mul2,result;
+    result.push_back(0);
+    for(;i1!=m1.end();i1++)
+        mul1.push_back(*i1-'0');
+    for(;i2!=m2.end();i2++)
+        mul2.push_back(*i2-'0');
+    vector<int>::iterator imul1(mul1.end()-1),imul2(mul2.end()-1),ir(result.begin());
+    vector<int>::difference_type i(0);
+    for(;imul1>=mul1.begin();imul1--)
+    {
+        for(imul2=mul2.end()-1;imul2>=mul2.begin();imul2--)
+        {
+            *ir+=*imul1**imul2;
+            if(ir==result.end()-1)
+                result.push_back(0);
+            ir=result.end()-1;
+        }
+        i++;
+        ir=result.begin()+i;
+    }
+    for(ir=result.end()-1;*ir==0;ir--)
+        ir=result.erase(ir);
+    for(ir=result.begin();ir!=result.end()-1;ir++)
+        if(*ir>9)
+        {
+            *(ir+1)+=*ir/10;
+            *ir%=10;
+        }
+    for(ir=result.end()-1;ir>=result.begin();ir--)
+        cout<<*ir;
+    cout<<endl;
+    return 0;
+}
