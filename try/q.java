048bea73ea09021404ea0fc579edc02503f3f588
Leoscott <PRCSteven06@gmail.com>
2022-03-03 20:16:42 +0800

    ok

---
+++ try/q.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class q{
+    public static void main(String args[])
+    {
+        Queue<String> qu=new LinkedList<>();
+        qu.add("Ni");
+        qu.add("Hao");
+        while(qu.size()>0)
+            System.out.println(qu.poll());
+    }
+}
