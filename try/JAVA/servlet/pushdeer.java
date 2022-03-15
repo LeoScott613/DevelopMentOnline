6cd69c12cf3e5f722564009f94c2ee1263f7be7e
LeoScott613 <87657181+LeoScott613@users.noreply.github.com>
2022-03-15 11:52:35 +0800

    push

--- try/JAVA/servlet/pushdeer.java
+++ try/JAVA/servlet/pushdeer.java
@@ -5,5 +5,9 @@ public class pushdeer extends HttpServlet {
     protected void doGet(HttpServletRequest request,HttpServletResponse response) {
         response.setContentType("text/html;charset:UTF-8");
         PrintWriter out=response.getWriter();
+        String content=request.getParameter("content");
+        String pushSite="https://api2.pushdeer.com/message/push?pushkey=%20PDU2524TD9q37WmTnU0ZMI7X4EEG8BEdj6TbCaKh&text="+content;
+        response.setStatus(response.SC_MOVED_TEMPORARILY);
+        response.setHeader("Location", pushSite); 
     }
 }
