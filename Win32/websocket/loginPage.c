6bdfeedc3b9166a19d1a41d8f3224d21acdbb07a
Leoscott <PRCSteven06@gmail.com>
2022-05-26 21:19:44 +0800

    loginpage as a empty shell

--- Win32/websocket/loginPage.c
+++ Win32/websocket/loginPage.c
@@ -5,8 +5,11 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE prevhinstance,PSTR pstr,int ncm
     mainwin.lpszClassName=_T("mainwin");
     mainwin.hInstance=hInstance;
     RegisterClass(&mainwin);
-    pos mainpos={300,300,700,700};
-    pos loginbutt={20,20,100,60};
+    pos mainpos={300,300,400,270};
+    pos loginbutt={130,160,100,45};
+    pos password={90,100,185,22};
+    pos account={90,30,185,22};
+
     HWND mainhand=CreateWindowExA(//main window
         0,
         mainwin.lpszClassName,
@@ -23,6 +26,24 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE prevhinstance,PSTR pstr,int ncm
         _T("login"),
         WS_VISIBLE | WS_CHILD,
         loginbutt.x,loginbutt.y,loginbutt.width,loginbutt.height,
+        mainhand,
+        NULL,hInstance,NULL
+    );
+    HWND pass=CreateWindowExA(
+        0,
+        WC_EDIT,
+        NULL,
+        WS_VISIBLE | WS_CHILD | ES_PASSWORD,
+        password.x,password.y,password.width,password.height,
+        mainhand,
+        NULL,hInstance,NULL
+    );
+    HWND acc_edit=CreateWindowExA(
+        0,
+        WC_EDIT,
+        NULL,
+        WS_VISIBLE | WS_CHILD,
+        account.x,account.y,account.width,account.height,
         mainhand,
         NULL,hInstance,NULL
     );
